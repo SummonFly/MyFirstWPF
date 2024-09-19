f84af019a767090bd1daf43bf78a176a9e4c48f1
SummonFly <109837346+SummonFly@users.noreply.github.com>
2024-09-19 11:07:52 +0900

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyFirstWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRegistration(object sender, RoutedEventArgs e)
+        {
+            string login = LoginBox.Text.Trim();
+            string password = FirstPasswordBox.Password.Trim();
+            string passwordConfirm = SecondPasswordBox.Password.Trim();
+            string email = EmailBox.Text.Trim().ToLower();
+
+            if(login.Length < 5)
+            {
+                LoginBox.ToolTip = "Uncorrect";
+                LoginBox.Background = Brushes.DarkRed;
+            }
+            else if (password.Length < 5)
+            {
+                FirstPasswordBox.ToolTip = "Uncorrect";
+                FirstPasswordBox.Background = Brushes.DarkRed;
+            }else if (passwordConfirm.Length < 5 || password != passwordConfirm)
+            {
+                SecondPasswordBox.ToolTip = "Uncorrect";
+                SecondPasswordBox.Background = Brushes.DarkRed;
+            }else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                EmailBox.ToolTip = "Uncorrect";
+                EmailBox.Background = Brushes.DarkRed;
+            }else
+            {
+                MessageBox.Show("Success");
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataBaseManager.GetManager();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyFirstWPF
+{
+    public class User
+    {
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        public string Email { get; private set; }
+
+        public User(string login, string password, string email)
+        {
+            Login = login;
+            Password = password;
+            Email = email;
+        }
+    }
+
+    public class DataBaseManager
+    {
+        public List<User> Users;
+
+        private static DataBaseManager _dataBase;
+        private string _connectionString = "Data Source=UserDataBase.db";
+        private SQLiteConnection _connection;
+
+        public static DataBaseManager GetManager()
+        {
+            if (_dataBase == null)
+            {
+                _dataBase = new DataBaseManager();
+            }
+            _dataBase.SaveDataBase();
+            return _dataBase;
+        }
+
+        private void LoadDataBase()
+        {
+            _connection.Open();
+
+            SQLiteCommand command = _connection.CreateCommand();
+
+            command.CommandText = "SELECT * FROM Users";
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Users.Add(new User(reader.GetString(1), reader.GetString(2), reader.GetString(3)));
+                }
+            }
+            _connection.Close();
+        }
+
+        private void SaveDataBase()
+        {
+            Users.Add(new User("Login", "Password", "Email"));
+
+            Users.Add(new User("Login", "Password", "Email"));
+            Users.Add(new User("Login", "Password", "Email"));
+
+            _connection.Open();
+
+            SQLiteCommand command = new SQLiteCommand();
+            command.Connection = _connection;
+
+
+            for(int i = 0; i < Users.Count; i++)
+            {
+                //command.CommandText = $"INSERT INTO Users (Login, Password, Email) VALUES('{Users[i].Login}', '{Users[i].Password}', '{Users[i].Email}')";
+                command.CommandText = $"INSERT INTO Users (Login, Password, Email) VALUES('Login', 'Password', 'Email')";
+                MessageBox.Show("Успешно " + command.ExecuteNonQuery().ToString ());
+            }
+
+            _connection.Close();
+            _connection.Dispose();
+        }
+
+        private DataBaseManager()
+        {
+            _connection = new SQLiteConnection(_connectionString);
+            Users = new List<User>();
+        }
+    }
+
+    
+}
